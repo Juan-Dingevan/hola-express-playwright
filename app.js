244de9e42d73e9aa86ddbe1c1a4905188766da75
Juan Dingevan <jidingevan@gmail.com>
2024-12-02 19:29:05 -0300

    Cambie de planeta a mundooo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hola Planeta!!');
+  res.send('Hola Mundoooo!!');
 });
 
 module.exports = app;
